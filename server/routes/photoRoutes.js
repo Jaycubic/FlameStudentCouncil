1f43fd8c9cd333f1e1e4aaef9aac007c133f2a10
Jofrey John <jofrey.joseph@flame.edu.in>
2025-08-19 23:44:34 +0530

    Impf

--- server/routes/photoRoutes.js
+++ server/routes/photoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require('../controllers/photoController');
 const { validateToken } = require('../middleware/auth');
 
-router.post('/upload', validateToken, ctrl.uploadPhoto);
-router.get('/:studentId', validateToken, ctrl.getPhoto);
+router.post('/upload', ctrl.uploadPhoto);
+router.get('/:studentId', ctrl.getPhoto);
 
 module.exports = router;
