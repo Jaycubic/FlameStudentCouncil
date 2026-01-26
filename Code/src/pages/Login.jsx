e76c546bc3e53f000c026047b79c6ea750fe09b6
Jaycubic <jofrey.joseph@flame.edu.in>
2026-01-26 18:19:18 +0530

    FLameAwards

--- Code/src/pages/Login.jsx
+++ Code/src/pages/Login.jsx
@@ -381,7 +381,7 @@ function Login() {
                 h="100px"
               />
               <Heading size="lg" mb={2}>
-                STUDENT COUNCIL
+                FLAME AWARDS
               </Heading>
               <Text color={textColor}>Please enter your credentials</Text>
             </Box>
