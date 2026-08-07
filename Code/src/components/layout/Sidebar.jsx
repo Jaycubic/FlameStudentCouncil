bedd6d8f81e127f67a63aa4c24863a75b8d7cb42
Jofrey John <jofrey.joseph@flame.edu.in>
2026-08-07 08:56:01 +0300

    Improved

--- Code/src/components/layout/Sidebar.jsx
+++ Code/src/components/layout/Sidebar.jsx
@@ -185,7 +185,7 @@ function Sidebar() {
     { name: 'Dashboard', href: '/', icon: HomeIcon, roles: ['admin', 'RC', 'user', 'Council', 'AdminLite'] },
     { name: 'Positions', href: '/positions', icon: QueueListIcon, roles: ['admin'] },
     { name: 'Applicants', href: '/applicants', icon: TableCellsIcon, roles: ['admin'] },
-    { name: 'Nominations', href: '/nominations', icon: TrophyIcon, roles: ['admin'] },
+    //{ name: 'Nominations', href: '/nominations', icon: TrophyIcon, roles: ['admin'] },
     { name: 'Time & Title', href: '/time-management', icon: CalendarIcon, roles: ['admin'] },
     { name: 'Election Form', href: '/election-form', icon: IdentificationIcon, roles: ['Student'] },
     //{ name: 'Settings', href: '/settings', icon: Cog6ToothIcon, roles: ['admin'] },
